2271dd39e358abd7ae005cd25bac686c2cc2e75a
Puneet Trivedi <puneettrivedi2feb@gmail.com>
2025-12-13 15:03:56 +0530

    addded signin for mobile menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -238,12 +238,12 @@ setAuthProvider();
                     rounded-md px-2 py-2`}
             >Add Property</Link
           >}
-          {!session && <button
-            className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4"
-          >
-             <FaGoogle className='text-white mr-2'/>
-            <span>Login or Register</span>
-          </button>}
+         {provider && Object.values(provider).map((provider,index)=>(<button key={index} onClick={()=>signIn(provider.id)}
+                className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+              >
+                <FaGoogle className='text-white mr-2'/>
+                <span>Login or Register</span>
+              </button>))} 
           
         </div>
       </div>}
